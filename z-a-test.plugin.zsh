41022dfcdd0610a3fdb931d433e2c861f188e476
Philipp Schmitt <pschmitt@users.noreply.github.com>
2021-11-19 23:20:11 +0100

    Remove non-ascii chars from filenames (#2)

--- z-a-test.plugin.zsh
+++ z-a-test.plugin.zsh
@@ -4,18 +4,18 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz →zp-test-handler …zp-test-make-targets
+autoload -Uz zp-test-handler zp-test-make-targets
 
 # An empty stub to fill the help handler fields
-→za-test-help-null-handler() { :; }
+za-test-help-null-handler() { :; }
 
 # Register atclone hook
 @zinit-register-annex "zinit-annex-test" hook:atclone-110 \
-    →zp-test-handler \
-    →za-test-help-null-handler \
+    zp-test-handler \
+    za-test-help-null-handler \
     "notest|test''" # register a new ice-mod: test''
 
 # Register atpull hook
 @zinit-register-annex "zinit-annex-test" hook:atpull-110 \
-    →zp-test-handler \
-    →za-test-help-null-handler
+    zp-test-handler \
+    za-test-help-null-handler
